2742399cf0ee47f63d7d1be65a6341b248866262
3nvi <aggelos@orfium.com>
2019-04-29 23:48:42 +0300

    added missing global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { injectGlobal } from 'emotion';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+   
+  html,
+  body,
+  #root {
+    font-family: 'Lato', sans-serif;
+    height: 100%;
+    font-size: 14px;
+    color: #888;
+    margin: 0;
+    padding: 0;
+  }
+  
+  button {
+    border: none;
+    background: transparent;
+    box-shadow: none;
+    text-shadow: none;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    cursor: pointer;
+    font-size: inherit;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
